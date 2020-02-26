41edaaa55749517d69c9ff6d66dff5608a4a0d39
Brandon Rozek <rozekbrandon@gmail.com>
2020-02-25 21:51:41 -0500

    Make variables, enforce framerate, and tune for low latency

--- camerastream/gstream_send.bash
+++ camerastream/gstream_send.bash
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! x264enc ! rtph264pay ! udpsink host=10.0.10.2 port=5000
+DEVICE=/dev/video0
+FRAMERATE=15
+DESTIP=10.0.10.2
+DESTPORT=5000
+gst-launch-1.0 \
+	v4l2src device=$DEVICE ! \
+	videoconvert ! \
+	videorate ! \
+	video/x-raw,framerate=$FRAMERATE/1 ! \
+	x264enc interlaced=true tune=zerolatency speed-preset=ultrafast ! \
+	rtph264pay ! \
+	udpsink host=$DESTIP port=$DESTPORT
