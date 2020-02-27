070b0719b9252035fce1f980856b1c36521d1e88
Brandon Rozek <rozekbrandon@gmail.com>
2020-02-27 00:22:21 -0500

    Proper python package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+# pyobject?
+#opencv
+# numpy
+setup(name="picar",
+      version="0.0.1",
+      packages=find_packages(),
+      install_requires = [
+          "numpy~=1.18.1",
+          "pycairo~=1.19.0",
+          "PyGObject~=3.34.0",
+          "opencv-contrib-python~=4.2.0.32"
+      ]
+)
