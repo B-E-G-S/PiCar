3bd66b3f87a634d8fad374be182cf84d45330c13
Brandon Rozek <rozekbrandon@gmail.com>
2020-02-27 00:13:21 -0500

    Made camerastream into a python library and made an example script

---
+++ camerastream/__init__.py
@@ -0,0 +1,1 @@
+from .gstream_receive import *
---
+++ test_video.py
@@ -0,0 +1,16 @@
+from camerastream import *
+import cv2
+from time import sleep
+
+def show_frame(frame):
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        return False
+    return True
+
+v = VideoReceive(show_frame)
+v.start()
+sleep(20)
+v.stop()
+
+cv2.destroyAllWindows()
