edb41402dc0f2786bdf8d4f52fb1c390804b4332
Benjamin Whipkey <whipkey.benjamin@outlook.com>
2020-05-16 17:38:37 -0400

    Added basic client code

--- picarconclient-qt/main_window.cpp
+++ picarconclient-qt/main_window.cpp
@@ -7,53 +7,64 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    //Connect angle controls to change with one another.
-    connect(ui->center_button, &QPushButton::pressed, [=]()
-    {
-        ui->angle_slider->setValue(90);
-    });
+	test_interface = new NetInterface("10.0.10.20");
 
-    connect(ui->angle_slider, &QSlider::valueChanged, [=](int value)
-    {
-       if(value != ui->angle_box->value())
-       {
-           ui->angle_box->setValue(value);
-       }
-    });
+	//Connect angle controls to change with one another.
+	connect(ui->center_button, &QPushButton::pressed, [=]()
+	{
+		ui->angle_slider->setValue(0);
+	});
 
-    connect(ui->angle_box, QOverload<int>::of(&QSpinBox::valueChanged), [=](int value)
-    {
-        if(value != ui->angle_slider->value())
-        {
-            ui->angle_slider->setValue(value);
-        }
-    });
+	connect(ui->angle_slider, &QSlider::valueChanged, [=](int value)
+	{
+	   if(value != ui->angle_box->value())
+	   {
+		   ui->angle_box->setValue(value);
+	   }
+	});
 
-    //Connect speed controls to change with one another.
-    connect(ui->stop_button, &QPushButton::pressed, [=]()
-    {
-        ui->speed_slider->setValue(0);
-    });
+	connect(ui->angle_box, QOverload<int>::of(&QSpinBox::valueChanged), [=](int value)
+	{
+		if(value != ui->angle_slider->value())
+		{
+			ui->angle_slider->setValue(value);
+		}
+	});
 
-    connect(ui->speed_slider, &QSlider::valueChanged, [=](int value)
-    {
-       if(value != ui->speed_box->value())
-       {
-           ui->speed_box->setValue(value);
-       }
-    });
+	//Connect speed controls to change with one another.
+	connect(ui->stop_button, &QPushButton::pressed, [=]()
+	{
+		ui->speed_slider->setValue(0);
+	});
 
-    connect(ui->speed_box, QOverload<int>::of(&QSpinBox::valueChanged), [=](int value)
-    {
-        if(value != ui->speed_slider->value())
-        {
-            ui->speed_slider->setValue(value);
-        }
-    });
+	connect(ui->speed_slider, &QSlider::valueChanged, [=](int value)
+	{
+	   if(value != ui->speed_box->value())
+	   {
+		   ui->speed_box->setValue(value);
+	   }
+	});
+
+	connect(ui->speed_box, QOverload<int>::of(&QSpinBox::valueChanged), [=](int value)
+	{
+		if(value != ui->speed_slider->value())
+		{
+			ui->speed_slider->setValue(value);
+		}
+	});
+
+	test_interface->pingServer();
+
+	connect(ui->test_packet_button, &QPushButton::pressed, [=]()
+	{
+		test_interface->sendState(ui->speed_box->value(), ui->angle_box->value());
+	});
+
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+	delete(test_interface);
+	delete(ui);
 }
 
--- picarconclient-qt/main_window.h
+++ picarconclient-qt/main_window.h
@@ -1,21 +1,34 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "net_interface.h"
+
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+
+namespace Ui
+{
+	class MainWindow;
+}
+
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
+
     Q_OBJECT
 
 public:
+
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
+
     Ui::MainWindow *ui;
+
+	NetInterface *test_interface;
+
 };
 #endif // MAINWINDOW_H
